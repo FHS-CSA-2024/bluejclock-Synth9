9c0b34502bad1c9c2d256c417713526bb7409a63
Synth9 <96803186+Synth9@users.noreply.github.com>
2024-09-05 19:09:21 -0700

    Update ClockDriver.java
    Finished ClockDriver

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,3 +1,5 @@
+package src.main.java;
+
 
 public class ClockDriver
 {
@@ -14,7 +16,38 @@ public class ClockDriver
         //  * Tick test for 01:59 to 02:00
         //  * Tick test for 09:59 to 10:00
         //  * Tick test for 23:59 to 00:00
+        ClockDisplay test1 = new ClockDisplay();
+        String test1Output = test1.getTime();
+        System.out.println("Empty Con. Test - " + test1Output);
         
+        ClockDisplay test2 = new ClockDisplay(3,45);
+        String test2Output = test2.getTime();
+        System.out.println("2 Arg. Con. Test - " + test2Output);
+        
+        ClockDisplay tickTest1 = new ClockDisplay(3,32);
+        System.out.println("\tTick Test 1 PreTick - " + tickTest1.getTime());
+        tickTest1.timeTick();
+        System.out.println("\tTick Test 1 PostTick - " + tickTest1.getTime());
+        
+        ClockDisplay tickTest2 = new ClockDisplay(3,9);
+        System.out.println("\tTick Test 2 PreTick - " + tickTest2.getTime());
+        tickTest2.timeTick();
+        System.out.println("\tTick Test 2 PostTick - " + tickTest2.getTime());
+        
+        ClockDisplay tickTest3 = new ClockDisplay(1,59);
+        System.out.println("\tTick Test 3 PreTick - " + tickTest3.getTime());
+        tickTest3.timeTick();
+        System.out.println("\tTick Test 3 PostTick - " + tickTest3.getTime());
+        
+        ClockDisplay tickTest4 = new ClockDisplay(9,59);
+        System.out.println("\tTick Test 4 PreTick - " + tickTest4.getTime());
+        tickTest4.timeTick();
+        System.out.println("\tTick Test 4 PostTick - " + tickTest4.getTime());
+        
+        ClockDisplay tickTest5 = new ClockDisplay(23,59);
+        System.out.println("\tTick Test 5 PreTick - " + tickTest5.getTime());
+        tickTest5.timeTick();
+        System.out.println("\tTick Test 5 PostTick - " + tickTest5.getTime());
         
         //Implement tests for your ClockDisplaySeconds
         //  * Test that you can do the basics
@@ -28,6 +61,34 @@ public class ClockDriver
         //  * Tick test for 01:59:59 to 02:00:00
         //  * Tick test for 23:59:59 to 00:00:00
         
+        ClockDisplaySeconds test3 = new ClockDisplaySeconds();
+        String test3Output = test3.getTime();
+        System.out.println("Empty Con. Test - " + test3Output);
+        
+        ClockDisplaySeconds test4 = new ClockDisplaySeconds(3,45,15);
+        String test4Output = test4.getTime();
+        System.out.println("3 Arg. Con. Test - " + test4Output);
+        
+        ClockDisplaySeconds tickTest6 = new ClockDisplaySeconds(3,32,59);
+        System.out.println("\tTick Test 6 PreTick - " + tickTest6.getTime());
+        tickTest6.timeTick();
+        System.out.println("\tTick Test 6 PostTick - " + tickTest6.getTime());
+        
+        ClockDisplaySeconds tickTest7 = new ClockDisplaySeconds(1,0,59);
+        System.out.println("\tTick Test 7 PreTick - " + tickTest7.getTime());
+        tickTest7.timeTick();
+        System.out.println("\tTick Test 7 PostTick - " + tickTest7.getTime());
+        
+        ClockDisplaySeconds tickTest8 = new ClockDisplaySeconds(1,59,59);
+        System.out.println("\tTick Test 8 PreTick - " + tickTest8.getTime());
+        tickTest8.timeTick();
+        System.out.println("\tTick Test 8 PostTick - " + tickTest8.getTime());
+        
+        ClockDisplaySeconds tickTest9 = new ClockDisplaySeconds(23,59,59);
+        System.out.println("\tTick Test 9 PreTick - " + tickTest9.getTime());
+        tickTest9.timeTick();
+        System.out.println("\tTick Test 9 PostTick - " + tickTest9.getTime());
+        
         /*CHALLENGE*/
         //Implement tests for your ClockDisplay12Hour
         //  * Test that you can do the basics
@@ -40,5 +101,33 @@ public class ClockDriver
         //  * Tick test for 11:59PM to 12:00AM
         //  * Tick test for 11:59AM to 12:00PM
         //  * Tick test for 12:59PM to 01:00PM
+        
+        ClockDisplay12Hour test5 = new ClockDisplay12Hour();
+        String test5Output = test5.getTime();
+        System.out.println("Empty Con. Test - " + test5Output);
+        
+        ClockDisplay12Hour test6 = new ClockDisplay12Hour(3,45);
+        String test6Output = test6.getTime();
+        System.out.println("2 Arg. Con. Test - " + test6Output);
+        
+        ClockDisplay12Hour tickTest10 = new ClockDisplay12Hour(15,32);
+        System.out.println("\tTick Test 10 PreTick - " + tickTest10.getTime());
+        tickTest10.timeTick();
+        System.out.println("\tTick Test 10 PostTick - " + tickTest10.getTime());
+        
+        ClockDisplay12Hour tickTest11 = new ClockDisplay12Hour(23,59);
+        System.out.println("\tTick Test 11 PreTick - " + tickTest11.getTime());
+        tickTest11.timeTick();
+        System.out.println("\tTick Test 11 PostTick - " + tickTest11.getTime());
+        
+        ClockDisplay12Hour tickTest12 = new ClockDisplay12Hour(11,59);
+        System.out.println("\tTick Test 12 PreTick - " + tickTest12.getTime());
+        tickTest12.timeTick();
+        System.out.println("\tTick Test 12 PostTick - " + tickTest12.getTime());
+        
+        ClockDisplay12Hour tickTest13 = new ClockDisplay12Hour(12,59);
+        System.out.println("\tTick Test 13 PreTick - " + tickTest13.getTime());
+        tickTest13.timeTick();
+        System.out.println("\tTick Test 13 PostTick - " + tickTest13.getTime());
     }
 }
